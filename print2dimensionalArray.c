68d4b93851987b4beea45508ecc50b09e51e1914
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-06 12:11:58 +0300

    Print a 2 dimensional array

---
+++ print2dimensionalArray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Starting point
+*
+*Return - 0 (Success)
+**/
+int main()
+{
+    int twodarr[2][3] = {{1,2,3},{4,5,6}};
+    int i, j;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", twodarr[i][j]);
+        }
+        printf("\n");
+    }
+    return (0);
+}
